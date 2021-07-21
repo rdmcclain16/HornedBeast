64f1a3129473a6e6f4ee9002699543f9779ab357
rdmcclain16 <74519618+rdmcclain16@users.noreply.github.com>
2021-07-20 20:27:31 -0500

    added feature 2 of lab 2

--- my-react-app/src/components/HornedBeasts.js
+++ my-react-app/src/components/HornedBeasts.js
@@ -1,6 +1,12 @@
 import React from 'react';
+const { useState } = React;
 
 function HornedBeasts({image_url, title, description}){
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
+  const [counter, setCounter] = useState(0);
+
   return(
     <>
       <h2>
@@ -8,6 +14,7 @@ function HornedBeasts({image_url, title, description}){
       </h2>
       <img height="500px" src={image_url} alt="Bird" title="Bird"/>
       <p>{description}</p>
+      <button onClick={handleClick}>Favorite {counter}</button>
     </>
   );
 }
