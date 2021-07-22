6ec5341b1ecc142d577030d8ac121d2341bc81fa
rdmcclain16 <74519618+rdmcclain16@users.noreply.github.com>
2021-07-21 21:27:38 -0500

    saving progress from lab2 feature 3

--- my-react-app/src/components/HornedBeasts.js
+++ my-react-app/src/components/HornedBeasts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillHeartFill } from 'react-icons/bs';
 const { useState } = React;
 
 function HornedBeasts({image_url, title, description}){
@@ -12,9 +13,11 @@ function HornedBeasts({image_url, title, description}){
       <h2>
         {title}
       </h2>
-      <img height="500px" src={image_url} alt="Bird" title="Bird"/>
+      <button><img onClick={handleClick} height="500px" width="500px" src={image_url} alt="Bird"></img>
+        <BsFillHeartFill/>
+        {counter}
+      </button>
       <p>{description}</p>
-      <button onClick={handleClick}>Favorite {counter}</button>
     </>
   );
 }
--- my-react-app/src/components/Main.js
+++ my-react-app/src/components/Main.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import { animalData } from '../data';
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col} from 'react-bootstrap';
 function Main() {
   return (
     <>
       {animalData.map((data, key) => {
         return (
-          <div key={key}>
-            <HornedBeasts
-              key={key}
-              image_url={data.image_url}
-              title={data.title}
-              description={data.description}
-            />
-          </div>
+          <Container>
+            <Row>
+              <Col>
+                <div key={key}>
+                  <HornedBeasts
+                    key={key}
+                    image_url={data.image_url}
+                    title={data.title}
+                    description={data.description}
+                  />
+                </div>
+              </Col>
+            </Row>
+          </Container>
         );
       })}
       {/* <HornedBeasts
         title="Bird"
         imgurl={Bird}
         desc="special bird"/> */}
+      <button variant="primary">Primary</button>
+      <button variant="secondary">Secondary</button>
+      <button variant="success">Sucess</button>
     </>
   );
 }
